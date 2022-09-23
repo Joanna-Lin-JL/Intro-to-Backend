0b9375cce039cd5c6f45b3fd527bd6ac04d1e18e
Joanna-Lin-JL <104398265+Joanna-Lin-JL@users.noreply.github.com>
2022-09-23 19:32:31 -0400

    Added GET, POST, DELETE requests
    GET, POST, DELETE for a specific post &
    GET, POST for a comment on a specific post

--- assignment1/app.py
+++ assignment1/app.py
@@ -1,4 +1,5 @@
 import json
+from tkinter import X
 
 from flask import Flask
 from flask import jsonify
@@ -21,8 +22,38 @@ posts = {
         "username": "alicia98"}
 }
 
+comments = {
+    0: {
+        2: {
+            "id": 2,
+            "upvotes": 8,
+            "text": "Wow, my first Reddit gold!",
+            "username": "alicia98"
+        },
+        3: {
+            "id": 3,
+            "upvotes": 5,
+            "text": "Wow!",
+            "username": "alicia98"
+        }},
+    1: {
+        4: {
+            "id": 4,
+            "upvotes": 8,
+            "text": "Wow, my first comment!",
+            "username": "alicia98"
+        },
+        5: {
+            "id": 5,
+            "upvotes": 100,
+            "text": "HAHA!",
+            "username": "alicia98"
+        }}
+}
+
+
 # Keep track of the next id
-post_id_counter = 2
+id_counter = 6
 
 
 @ app.route("/")
@@ -39,6 +70,91 @@ def get_all_posts():
         "posts": list(posts.values())
     }
     return json.dumps(res), 200
+
+
+@ app.route("/posts/", methods=["POST"])
+def create_post():
+    """
+    Create a post
+    """
+    global id_counter
+    body = json.loads(request.data)
+    title = body.get("title")
+    link = body.get("link")
+    username = body.get("username")
+    # Put all data into a dictionary that contains all info of the post
+    post = {
+        "id": id_counter,
+        "upvotes": 0,
+        "title": title,
+        "link": link,
+        "username": username
+    }
+    posts[id_counter] = post
+    id_counter += 1
+    return json.dumps(post), 201
+
+
+@app.route("/posts/<int:post_id>/")
+def get_post(post_id):
+    """
+    Get post by id
+    """
+    post = posts.get(post_id)
+    if post is None:
+        return json.dumps({"error": "Post not found!"}), 404
+    return json.dumps(post), 200
+
+
+@app.route("/posts/<int:post_id>/", methods=["DELETE"])
+def delete_post(post_id):
+    """
+    Delete post by id
+    """
+    post = posts.get(post_id)
+    if post is None:
+        return json.dumps({"error": "Post not found!"}), 404
+    del posts[post_id]
+    return json.dumps(post), 200
+
+
+@ app.route("/posts/<int:post_id>/comments/")
+def get_all_comments(post_id):
+    """
+    Return all comments of a specific post
+    """
+    post = posts.get(post_id)
+    if post is None:
+        return json.dumps({"error": "Post not found!"}), 404
+
+    res = {
+        "comments": list(comments[post_id].values())
+    }
+    return json.dumps(res), 200
+
+
+@ app.route("/posts/<int:post_id>/comments/", methods=["POST"])
+def create_comment(post_id):
+    """
+    Create a comment for a post
+    """
+    global id_counter
+    body = json.loads(request.data)
+    text = body.get("text")
+    username = body.get("username")
+    # Put all data into a dictionary that contains all info of the post
+    comment = {
+        "id": id_counter,
+        "upvotes": 0,
+        "text": text,
+        "username": username
+    }
+    post = posts.get(post_id)
+    if post is None:
+        return json.dumps({"error": "Post not found!"}), 404
+    comments[post_id][id_counter] = comment
+    id_counter += 1
+    return json.dumps(comment), 201
 
 
 # your routes here
